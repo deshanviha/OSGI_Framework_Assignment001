5cabd0073f8a1705dc12adb0903934f6d28b6715
Vihanga Deshan <58096787+deshanviha@users.noreply.github.com>
2022-03-14 21:55:01 +0530

    Delete Soft_Drink_Publisher directory

--- Soft_Drink_Publisher/src/softdrink_publisher/softDrinkPublisher.java
+++
@@ -1,9 +0,0 @@
-package softdrink_publisher;
-
-public interface softDrinkPublisher {
-
-	public void displaySoftDrink(int a);
-	public double getPrice(int type, int softDrink);
-	public String getName(int type, int softDrink);
-	
-}
