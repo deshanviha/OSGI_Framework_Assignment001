279f238bcefbe5bde9bd6a59ae564610980e6673
Vihanga Deshan <58096787+deshanviha@users.noreply.github.com>
2022-03-14 21:54:18 +0530

    Delete Hot_Drink_Publisher directory

--- Hot_Drink_Publisher/src/Hot_Drink_Publisher/hotDrinkPublisher.java
+++
@@ -1,9 +0,0 @@
-package Hot_Drink_Publisher;
-
-public interface hotDrinkPublisher {
-
-	public void getHotDrink(int a);
-	public double getHotDrinkPrice(int type, int hotDrink);
-	public String getHotDrinkName(int type, int hotDrink);
-	public void getlocation();
-}
